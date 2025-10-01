82cae3ce389c0679ff4805c3221519e1e6df6af2
Aadit Singh <aadit.singh2023@vitstudent.ac.in>
2025-10-01 21:35:04 +0530

    Update HeuristicVmAllocationPolicy.java

--- Heuristic/HeuristicVmAllocationPolicy.java
+++ Heuristic/HeuristicVmAllocationPolicy.java
@@ -26,16 +26,16 @@ public class HeuristicVmAllocationPolicy extends VmAllocationPolicySimple {
         Host bestHost = null;
         double minUtilization = Double.MAX_VALUE;
 
-        // Loop through all available hosts
+        
         for (Host host : getHostList()) {
-            // Check if the host has enough resources for the VM
+           
             if (host.isSuitableForVm(vm)) {
                 
                 
-                // Calculate utilization by checking how many MIPS are in use
+                
                 double utilization = (host.getTotalMips() - host.getVmScheduler().getAvailableMips()) / host.getTotalMips();
 
-                // If this host is less busy than the best one found so far, it's the new best
+                
                 if (utilization < minUtilization) {
                     minUtilization = utilization;
                     bestHost = host;
@@ -44,4 +44,5 @@ public class HeuristicVmAllocationPolicy extends VmAllocationPolicySimple {
         }
         return bestHost;
     }
+
 }
